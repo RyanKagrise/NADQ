4e271c7d089790320b8ca7af8fe1ec0729b0d7f9
Ryan Kagrise <rdkcode@gmail.com>
2022-02-14 14:23:47 -1000

    Added users login route/post for authentification and added auth.js for helper functions

--- express-project-starter/app.js
+++ express-project-starter/app.js
@@ -8,9 +8,12 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const { restoreUser } = require('./auth');
+
+const { v4:uuidv4 } = require('uuid')
 
 const app = express();
-
+const secret = uuidv4();
 // view engine setup
 app.set('view engine', 'pug');
 
@@ -25,12 +28,14 @@ const store = new SequelizeStore({ db: sequelize });
 
 app.use(
   session({
-    secret: 'superSecret',
+    secret: `${secret}`,
     store,
     saveUninitialized: false,
     resave: false,
   })
 );
+
+app.use(restoreUser);
 
 // create Session table if it doesn't already exist
 store.sync();
--- express-project-starter/routes/users.js
+++ express-project-starter/routes/users.js
@@ -1,9 +1,67 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const db = require('../db/models');
+const { csrfProtection, asyncHandler } = require('./utils')
+const { loginUser, logoutUser } = require('../auth');
+
+const router = express.Router();
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/users/login', csrfProtection, (req, res) => {
+  res.render('userLogin', {
+    title:'Login',
+    csrfToken: req.csrfToken(),
+  })
 });
+
+const loginValidator = [
+  check('emailAddress')
+  .exists({ checkFalsy: true })
+  .withMessage('Please provide a correct email address!'),
+  check('password')
+  .exists({ checkFalsy: true })
+  .withMessage('Please provide a correct password value!')
+];
+
+router.post('/users/login', csrfProtection, loginValidator, asyncHandler(async(req,res) => {
+  const {
+    emailAddress,
+    password,
+  } = req.body;
+
+  let errors = [];
+  const validatorErrors = validationResult(req);
+
+  if (validatorErrors.isEmpty()) {
+    const user = await db.User.findOne({ where: { emailAddress }});
+
+  if (user !== null) {
+    const passwordCheck = await bcrypt.compare(password, user.hashedPassword.toString());
+
+    if (passwordCheck) {
+      loginUser(req, res, user);
+      return res.redirect('/');
+    }
+  }
+
+  errors.push('Login attempt has failed for provided email address and password ya dummy!');
+} else {
+  errors = validatorErrors.array().map((error) => error.msg);
+}
+
+  res.render('userLogin', {
+    title: 'Login',
+    emailAddress,
+    errors,
+    csrfToken: req.csrfToken(),
+  });
+}))
+
+router.post('/users/logout', (req, res) => {
+  logoutUser(req, res);
+  res.redirect('/users/login');
+})
 
 module.exports = router;

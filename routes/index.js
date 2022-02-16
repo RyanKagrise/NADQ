2ad399807283b702e908ca29a780f274e9a72329
Ryan Kagrise <rdkcode@gmail.com>
2022-02-16 09:10:37 -1000

    added home pug and added mixin and added topic container with links

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 
 const { requireAuth } = require('../auth')
-
+const db = require('../db/models')
 const router = express.Router();
 
 const { csrfProtection, asyncHandler } = require('./utils')
 
 /* GET home page. */
-router.get('/', requireAuth, (req, res, next) => {
-  res.render('index', { title: 'a/A Express Skeleton Home' });
-});
+router.get('/', requireAuth, asyncHandler( async(req, res, next) => {
+
+  const topics = await db.Topic.findAll();
+  
+  res.render('home', { title: 'Never A Dumb Question!', topics });
+}));
 
 module.exports = router;

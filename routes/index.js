73c3e9041b064f3a9a5727e4508b6badd059f396
LincolnHer <lincolnher15@gmail.com>
2022-02-16 10:19:54 -0700

    added logout and require auth to splash page

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
+const { requireAuth } = require('../auth')
 
 const router = express.Router();
 
 const { csrfProtection, asyncHandler } = require('./utils')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', requireAuth, (req, res, next) => {
   res.render('index', { title: 'a/A Express Skeleton Home' });
 });
 
